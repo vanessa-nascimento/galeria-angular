e3fb2a9f5c7342d7ba2056cd3412e7e54819868f
Vanessa Nascimento <vanascimento@uolinc.com>
2020-05-21 16:29:37 -0300

    upload do projeto

---
+++ alurapic/src/app/photos/photo/photo.ts
@@ -0,0 +1,9 @@
+export interface Photo{
+    id: number,
+    postDate: Date,
+    description: string,
+    allowComments: boolean,
+    likes: number,
+    comments: number,
+    userId: number
+}
